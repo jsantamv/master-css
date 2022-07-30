1df5e104b2f95a9870c8b39d83a0b6044746ff15
Juan Carlos Santamaria V <jsantamv@live.com>
2022-07-29 19:39:42 -0600

    actualizacion

--- maquetacion-2-responsive/modules/devices.js
+++ maquetacion-2-responsive/modules/devices.js
@@ -3,5 +3,7 @@ export const devicesTypeIOS = () => {
     let deviceType = navigator.userAgent.match(/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Kindle|Silk/i)
     let name = deviceType[0];
 
-    console.log(name, deviceType);
+    let result = navigator.userAgent.includes('iPad')
+
+    console.log(result, name, deviceType);
 }
